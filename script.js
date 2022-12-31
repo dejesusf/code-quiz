48ac1cce44f464f3a92636c12af6b73724778cf3
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2022-12-30 21:27:31 -0800

    fixed details page

--- script.js
+++ script.js
@@ -16,12 +16,50 @@ var questions= [
     answers:["Greater than", "Strict equality", "Less Than", "Not"],
     correctAnswer:"Not"}
 ]
-var startButton=document.querySelector("#start");
-var timer=document.querySelector("#timer");
+var start=document.getElementById("start");
+var timer=document.getElementById("timer");
+var details=document.getElementById("details");
+var quiz=document.getElementById("quiz");
+var correct=document.getElementById("correct");
+var scoreboard=document.getElementById("scoreboard")
+var currentQuestion=0;
 var secondsLeft;
+var timerInterval;
+var shuffleQs;
+
+//randomize questions - Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+    for (var i=array.length-1; i>0; i--){
+        var j=Math.floor(Math.random()*(i+1));
+        var temp=array[i];
+        array [i]=array[j];
+        array[j]=temp;
+    }
+    return array;
+}
 
 //start quiz
 function startQuiz(){
+    details.setAttribute("class","hidden")
+    quiz.setAttribute("class","")
+    shuffleQs=shuffleArray(questions)
+    secondsLeft=60;
+    timer.textContent=secondsLeft + " second(s) remaining!"
+    timerInterval=setInterval(()=>{
+        secondsLeft--;
+        timer.textContent=secondsLeft + " second(s) remaining!"
+        if (secondsLeft<=0){
+            endQuiz()
+        }
+    },1000);
+    nextQuestion()
+}
+
+//next question
+function nextQuestion(){
+    question.setAttribute("class","");
+    answers.setAttribute("class","");
+    correct.setAttribute("class","hidden");
     
 }
 
@@ -29,7 +67,13 @@ function startQuiz(){
 
 //incorrect answer & subtract time
 
+//game over
+function endQuiz(){
+    clearInterval(timerInterval);
+    quiz.setAttribute("class","hidden");
+
+}
 //highscore
 
-//event listeners
-startButton.addEventListener("click", startQuiz)
+//event listener
+start.addEventListener("click",startQuiz);
